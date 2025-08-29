9f43d2e8a0a42a767806f417cbfd92b91a914418
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-08-29 09:05:05 +0500

    Added T 08 Auth System

--- T 08 Auth System with Passwd Checker/main.py
+++ T 08 Auth System with Passwd Checker/main.py
@@ -1,3 +1,29 @@
+# Registration Function
+def register_user():
+    print("*********Submit the Inputs to Register**********")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    with open('user_credentials.txt', 'a') as file:
+        file.write(f"{username}:{password}\n")
+    print("User Registration was Successfull.")
+
+# Login Function
+def login_user():
+    print("*********Welcome to Login View**********")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    with open('user_credentials.txt', 'r') as file:
+        users = file.readlines()
+        for user in users:
+            stored_username, stored_password = user.strip().split(':')
+            if username == stored_username and password == stored_password:
+                print("Login Successfull.")
+                return
+        print("Invalid username or password!")
+        
+
 def main():
     while True:
         print('Welcome to User Registration System')
@@ -5,15 +31,15 @@ def main():
         print('2. Login')
         print('3. Exit')
         choice = input('What would you like to do?: ')
-        
-        if choice == '1':
-            print("Register Selected!")
-        elif choice == '2':
-            print("Login Selected!")
-        elif choice == '3':
-            print("Exiting!")
-            break
-        else:
-            print("Invalid Choice, Please try again!")
+        match choice:
+            case '1':
+                register_user()
+            case '2':
+                login_user()
+            case '3':
+                print("Exiting!")
+                break
+            case _:
+                print("Invalid Choice, Please try again!")
 
 main();
